3291e15b33ee49bb926d319cb7cdb8eade8ffb66
osheafm <osheafm@s.dcsdk12.org>
2021-03-23 11:30:15 -0600

    Initial Release

--- app/src/main/java/com/example/mystory/page1.java
+++ app/src/main/java/com/example/mystory/page1.java
@@ -2,7 +2,12 @@ package com.example.mystory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class page1 extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class page1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
+        Button nextLevelButton = findViewById(R.id.button);
+        nextLevelButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(getApplicationContext(), page2.class);
+                startActivity(intent);
+
+                return false;
+            }
+        });
     }
 }
